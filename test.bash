3db585ab2b3d16590bfa7f211356c4d297752e37
Kota Iwasaki <s24C1017db@s.chibakoudai.jp>
2025-10-30 13:07:19 +0900

    CHANG2

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX ...
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,8 +8,18 @@ ng () {
 
 res=0
 
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1]      || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1]      || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
